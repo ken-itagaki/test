0f1cb2b4b6f634437915ddd2d9e91987f299cac1
ken-itagaki <ken1024mimi0425@gmail.com>
2023-04-27 16:45:33 +0900

    login

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
 from lxml import html
+import mechanicalsoup
 
 url_list = ["https://www.python.jp/",
-            "https://www.ruby-lang.org/ja/"]
+            "https://www.ruby-lang.org/ja/",
+            "https://github.com/login"]
+
+a=0
+username = "ken-itagaki"
+password = "Kenken99ken"
 
 for url in url_list:
     
@@ -14,23 +20,49 @@ for url in url_list:
     if url == "https://www.python.jp/":
 
         text = tree.xpath('//*[@id="content-base"]/div/div/div[2]/div[1]/div/div[1]/h2/text()')[0]
-        print(text)
         if text != "Python3 ドキュメント":
             print("コンフルの値1 is wrong")
+            a=1
 
         text = tree.xpath('//*[@id="1geNZn"]/a[1]/text()')[0]
-        print(text)
         if text != "Discordサーバ":
             print("コンフルの値2 is wrong")
+            a=1
         
     
     elif url == "https://www.ruby-lang.org/ja/":
 
         text = tree.xpath('//*[@id="sidebar"]/div[1]/h3/strong/text()')[0]
-        print(text)
         if text !=  "はじめよう!":
             print("コンフルの値3 is wrong")
+            a=1
 
         text = tree.xpath('//*[@id="content"]/div[2]/p[1]/text()')[0]
-        if text !=  "コンフルの値":
-            print("コンフルの値4 is wrong")
+        if text !=  "Ruby 3.1.4 がリリースされました。":
+            print(url, "コンフルの値4 is wrong")
+            a=1
+
+    elif url == "https://github.com/login":
+
+        page_url = "https://github.com/settings/profile"
+        browser = mechanicalsoup.Browser()
+
+        login_page = browser.get("https://github.com/login")
+
+        login_form = login_page.soup.select("form")[0]
+        username_field = login_form.select("#login_field")[0]
+        password_field = login_form.select("#password")[0]
+        username_field["value"] = username
+        password_field["value"] = password
+
+        browser.submit(login_form, login_page.url)
+
+        response = requests.get(page_url)
+        #tree = html.fromstring(response.content)
+        #text = tree.xpath('/html/body/div[1]/div[6]/main/div/div[1]/div/div/h1/a/text()')[0]
+        text = response.text
+        print(text)
+
+
+if a == 0:
+    print("ok")
