01fc103bac6f82f18178de030734360138738131
ken-itagaki <ken1024mimi0425@gmail.com>
2023-04-27 10:15:39 +0900

    testOK

--- test.py
+++ test.py
@@ -1,33 +1,36 @@
 from bs4 import BeautifulSoup
 import requests
+from lxml import html
 
-url_list = ["https://url1", 
-            "https://url2", 
-            "https://url3"]
+url_list = ["https://www.python.jp/",
+            "https://www.ruby-lang.org/ja/"]
 
 for url in url_list:
     
     response = requests.get(url)
 
-    soup = BeautifulSoup(response.content, "html.parser")
+    tree = html.fromstring(response.content)
 
-    if url == "https://url1":
+    if url == "https://www.python.jp/":
 
-        text = soup.select('コンフルの値のパス/text()')[0]
-        if text != "コンフルの値":
-            print("コンフルの値 is wrong")
+        text = tree.xpath('//*[@id="content-base"]/div/div/div[2]/div[1]/div/div[1]/h2/text()')[0]
+        print(text)
+        if text != "Python3 ドキュメント":
+            print("コンフルの値1 is wrong")
 
-        text = soup.select('コンフルの値のパス/text()')[0]
-        if text != "コンフルの値":
-            print("コンフルの値 is wrong")
+        text = tree.xpath('//*[@id="1geNZn"]/a[1]/text()')[0]
+        print(text)
+        if text != "Discordサーバ":
+            print("コンフルの値2 is wrong")
         
     
-    elif url == "https://url2":
+    elif url == "https://www.ruby-lang.org/ja/":
 
-        text = soup.select('コンフルの値のパス/text()')[0]
-        if text !=  "コンフルの値":
-            print("コンフルの値 is wrong")
+        text = tree.xpath('//*[@id="sidebar"]/div[1]/h3/strong/text()')[0]
+        print(text)
+        if text !=  "はじめよう!":
+            print("コンフルの値3 is wrong")
 
-        text = soup.select('コンフルの値のパス/text()')[0]
+        text = tree.xpath('//*[@id="content"]/div[2]/p[1]/text()')[0]
         if text !=  "コンフルの値":
-            print("コンフルの値 is wrong")
+            print("コンフルの値4 is wrong")
